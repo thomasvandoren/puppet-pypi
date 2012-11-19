74cd7887c1f449c4043f1b55e832be85fc404dba
Thomas Van Doren <thomas.vandoren@gmail.com>
2012-11-19 14:02:27 -0800

    Add a spec test for default params.

--- spec/classes/pypi_spec.rb
+++ spec/classes/pypi_spec.rb
@@ -2,4 +2,52 @@ require 'spec_helper'
 
 describe 'pypi', :type => 'class' do
 
+  context "On Debian system with default params" do
+
+    let :facts do
+      {
+        :osfamily => 'Debian'
+      }
+    end # let
+
+    it do
+      should contain_group('pypi').with_ensure('present')
+      should contain_user('pypi').with(:ensure  => 'present',
+                                       :gid     => 'pypi')
+
+      should contain_file('/var/pypi').with(:ensure => 'directory',
+                                            :owner  => 'pypi',
+                                            :group  => 'pypi')
+      should contain_file('/var/pypi/packages').with(:ensure => 'directory',
+                                            :owner  => 'pypi',
+                                            :group  => 'pypi')
+      should contain_file('pypiserver_wsgi.py').with(:ensure => 'present',
+                                                     :path   => '/var/pypi/pypiserver_wsgi.py',
+                                                     :owner  => 'pypi',
+                                                     :group  => 'pypi',
+                                                     :mode   => '0755',
+                                                     :source => 'puppet:///modules/pypi/pypiserver_wsgi.py')
+      should contain_file('.htaccess').with(:ensure => 'present',
+                                            :path   => '/var/pypi/.htaccess',
+                                            :owner  => 'pypi',
+                                            :group  => 'pypi',
+                                            :mode   => '0755',
+                                            :source => 'puppet:///modules/pypi/.htaccess')
+
+      should contain_apache
+      should contain_apache__mod__wsgi
+      should contain_apache__vhost('pypi').with(:priority      => '10',
+                                               :port          => '80',
+                                               :docroot       => '/var/pypi',
+                                               :docroot_owner => 'pypi',
+                                               :docroot_group => 'pypi',
+                                               :template      => 'pypi/vhost-pypi.conf.erb')
+
+      should contain_package('python-pip').with_ensure('present')
+      should contain_package('passlib').with(:ensure   => 'present',
+                                             :provider => 'pip')
+      should contain_package('pypiserver').with(:ensure   => 'present',
+                                                :provider => 'pip')
+    end # it
+  end # context
 end # describe
